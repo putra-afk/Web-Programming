3f66dc36cfdde768a6b05323dcfe57920d7544e2
putra-afk <putralaksmanabayu@gmail.com>
2024-10-24 13:24:10 +0700

    update

--- exam/index.php
+++ exam/index.php
@@ -14,20 +14,36 @@
 <body>
 
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <a class="navbar-brand" href="index.php">Hostel Hotel & Resort</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="checkprice.php">Check Price</a></li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="checkprice.php">Check Price</a>
+                </li>
+
+                <!-- Display Logout if the user is logged in -->
                 <?php if (isset($_SESSION['username'])): ?>
-                    <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
                 <?php else: ?>
-                    <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
                 <?php endif; ?>
             </ul>
         </div>
     </nav>
+
+    <!-- Bootstrap JS (Optional for navbar toggle) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Slider -->
     <div id="hotelCarousel" class="carousel slide" data-ride="carousel">
--- exam/logout.php
+++ exam/logout.php
@@ -1,5 +1,20 @@
 <?php
+// Start the session
 session_start();
-session_destroy();
-header("Location: login.php");
-exit();
+
+// Check if the user is logged in
+if (isset($_SESSION['username'])) {
+    // Unset all session variables
+    session_unset();
+
+    // Destroy the session
+    session_destroy();
+
+    // Redirect to login page
+    header("Location: login.php");
+    exit();
+} else {
+    // If no session exists, redirect to login
+    header("Location: logout.php");
+    exit();
+}
